3af5a50b169fab168d705013c7bd0d8e73a860a0
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-12 14:09:57 +0300

    feat: add SplitScreen component to App

--- react-patterns-app/src/App.js
+++ react-patterns-app/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+// Components
+import SplitScreen from './SplitScreen';
+
 // Styles
 import './App.css';
 
 function App() {
-  return <></>;
+  return <SplitScreen />;
 }
 
 export default App;
