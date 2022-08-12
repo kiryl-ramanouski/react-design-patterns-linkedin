1deef7ec4ce77e6a0c087b0c3a55a6c8f8845f74
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-12 16:31:16 +0300

    refactor: clear App and add work data to it

--- react-patterns-app/src/App.js
+++ react-patterns-app/src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
 
-// Components
-import SplitScreen from './SplitScreen';
-
 // Styles
 import './App.css';
 
-const LeftHandComponent = ({ name }) => {
-  return <h1 style={{ backgroundColor: 'green' }}>{name}</h1>;
-};
+// Data
+const people = [
+  {
+    name: 'John Doe',
+    age: 54,
+    hairColor: 'brown',
+    hobbies: ['swimming', 'bicycling', 'video games'],
+  },
+  {
+    name: 'Brenda Smith',
+    age: 33,
+    hairColor: 'black',
+    hobbies: ['golf', 'mathematics'],
+  },
+  {
+    name: 'Jane Garcia',
+    age: 27,
+    hairColor: 'blonde',
+    hobbies: ['biology', 'medicine', 'gymnastics'],
+  },
+];
 
-const RightHandComponent = ({ message }) => {
-  return <p style={{ backgroundColor: 'red' }}>{message}</p>;
-};
+const products = [
+  {
+    name: 'Flat-Screen TV',
+    price: '$300',
+    description: 'Huge LCD screen, a great deal',
+    rating: 4.5,
+  },
+  {
+    name: 'Basketball',
+    price: '$10',
+    description: 'Just like the pros use',
+    rating: 3.8,
+  },
+  {
+    name: 'Running Shoes',
+    price: '$120',
+    description: 'State-of-the-art technology for optimum running',
+    rating: 4.2,
+  },
+];
 
 function App() {
-  return (
-    <SplitScreen leftWeight={1} rightWeight={3}>
-      <LeftHandComponent name='Shaun' />
-      <RightHandComponent message='Hello' />
-    </SplitScreen>
-  );
+  return <></>;
 }
 
 export default App;
