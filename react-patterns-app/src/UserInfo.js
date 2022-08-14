ae294d8b898f3c6166cb5c6b13c11dff83ef850e
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-14 20:24:51 +0300

    fix: fix UserInfo destructuring

--- react-patterns-app/src/UserInfo.js
+++ react-patterns-app/src/UserInfo.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 export const UserInfo = ({ user }) => {
-  const [name, age, hairColor, hobbies] = user || {};
+  const { name, age, hairColor, hobbies } = user || {};
+
   return user ? (
     <>
-      <h3>NameL {name}</h3>
+      <h3>{name}</h3>
       <p>Age: {age} years</p>
       <p>Hair Color: {hairColor}</p>
       <h3>Hobbies:</h3>
