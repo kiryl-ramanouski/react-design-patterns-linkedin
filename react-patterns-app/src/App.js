0dbb1f6aa68d6e3f49c51bc3b6ff9d9ddf66f148
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-15 19:54:37 +0300

    refactor: clear App

--- react-patterns-app/src/App.js
+++ react-patterns-app/src/App.js
@@ -1,38 +1,7 @@
 import React from 'react';
 
-// Rest API
-import axios from 'axios';
-
-// Components
-import { UserInfo } from './UserInfo';
-import { DataSource } from './DataSource';
-
-const getServerData = (url) => async () => {
-  const response = await axios.get(url);
-  return response.data;
-};
-
-const getLocalStorageData = (key) => () => {
-  return localStorage.getItem(key);
-};
-
-const Text = ({ message }) => <h1>{message}</h1>;
-
 function App() {
-  return (
-    <>
-      <DataSource getDataFunc={getServerData('/users/123')} resourceName="user">
-        <UserInfo />
-      </DataSource>
-
-      <DataSource
-        getDataFunc={getLocalStorageData('message')}
-        resourceName="message"
-      >
-        <Text />
-      </DataSource>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
