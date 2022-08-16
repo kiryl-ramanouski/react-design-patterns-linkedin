af3db02c6f00143c5bc463bb0349d60c18178631
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-16 15:34:26 +0300

    refactor: refactor buttons inside App

--- react-patterns-app/src/App.js
+++ react-patterns-app/src/App.js
@@ -6,37 +6,19 @@ import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
 const StepOne = ({ goToNext }) => (
   <>
     <h1>Step One</h1>
-    <button
-      onClick={() => {
-        goToNext({ name: 'John Doe' });
-      }}
-    >
-      Next
-    </button>
+    <button onClick={() => goToNext({ name: 'John Doe' })}>Next</button>
   </>
 );
 const StepTwo = ({ goToNext }) => (
   <>
     <h1>Step Two</h1>
-    <button
-      onClick={() => {
-        goToNext({ age: 100 });
-      }}
-    >
-      Next
-    </button>
+    <button onClick={() => goToNext({ age: 100 })}>Next</button>
   </>
 );
 const StepThree = ({ goToNext }) => (
   <>
     <h1>Step Tree</h1>
-    <button
-      onClick={() => {
-        goToNext({ hairColor: 'brown' });
-      }}
-    >
-      Next
-    </button>
+    <button onClick={() => goToNext({ hairColor: 'brown' })}>Next</button>
   </>
 );
 
