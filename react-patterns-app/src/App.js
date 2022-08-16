1503b69c838fdf5fb0e07eb91002a58c68e16d6c
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-16 18:11:21 +0300

    feat: add printProps() HOC and use it with UserInfo inside App

--- react-patterns-app/src/App.js
+++ react-patterns-app/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+// Components
+import { UserInfo } from './UserInfo';
+
+// HOC
+import { printProps } from './printProps';
+
+const UserInfoWrapped = printProps(UserInfo);
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <UserInfoWrapped a={1} b="Hello" c={{ name: 'Shaun' }} />
+    </>
+  );
 }
 
 export default App;
