636344ffd89cc2240be01e5a6ef14e8ac625ac0c
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-16 13:26:30 +0300

    refactor: clear App

--- react-patterns-app/src/App.js
+++ react-patterns-app/src/App.js
@@ -1,24 +1,7 @@
-import React, { useState } from 'react';
-
-// Components
-import { ControlledModal } from './ControlledModal';
+import React from 'react';
 
 function App() {
-  const [shouldShow, setShouldShow] = useState(false);
-
-  return (
-    <>
-      <ControlledModal
-        shouldShow={shouldShow}
-        onRequestClose={() => setShouldShow(false)}
-      >
-        <h1>Modal Opened</h1>
-      </ControlledModal>
-      <button onClick={() => setShouldShow(true)}>
-        {shouldShow ? 'Hide Modal' : 'Show Modal'}
-      </button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
