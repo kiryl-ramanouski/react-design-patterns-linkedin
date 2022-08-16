2488a562d04aa18f9519f2fc7c1b0afebb093e24
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-16 14:04:07 +0300

    feat: add UncontrolledOnboardingFlow to App and use it with children

--- react-patterns-app/src/App.js
+++ react-patterns-app/src/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
+// Components
+import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
+
+const StepOne = ({ goToNext }) => (
+  <>
+    <h1>Step One</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+);
+const StepTwo = ({ goToNext }) => (
+  <>
+    <h1>Step Two</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+);
+const StepThree = ({ goToNext }) => (
+  <>
+    <h1>Step Tree</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+);
+
 function App() {
-  return <></>;
+  return (
+    <UncontrolledOnboardingFlow>
+      <StepOne />
+      <StepTwo />
+      <StepThree />
+    </UncontrolledOnboardingFlow>
+  );
 }
 
 export default App;
