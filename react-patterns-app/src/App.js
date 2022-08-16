0fb42ad387197d1a2d7a3ec96193371092dec899
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-16 18:43:36 +0300

    feat: add withUser HOC to app and wrap withUser inside it

--- react-patterns-app/src/App.js
+++ react-patterns-app/src/App.js
@@ -4,14 +4,14 @@ import React from 'react';
 import { UserInfo } from './UserInfo';
 
 // HOC
-import { printProps } from './printProps';
+import { withUser } from './withUser';
 
-const UserInfoWrapped = printProps(UserInfo);
+const UserInfoWithLoader = withUser(UserInfo, '234');
 
 function App() {
   return (
     <>
-      <UserInfoWrapped a={1} b="Hello" c={{ name: 'Shaun' }} />
+      <UserInfoWithLoader />
     </>
   );
 }
