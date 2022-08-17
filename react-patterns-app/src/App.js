b83880cb3c2aa9c8958693e558222d1bb1a725e6
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-17 15:41:54 +0300

    feat: replace useCurrentUser() on new useUser() inside UserInfo and use it in App

--- react-patterns-app/src/App.js
+++ react-patterns-app/src/App.js
@@ -3,7 +3,13 @@ import React from 'react';
 import { UserInfo } from './UserInfo';
 
 function App() {
-  return <UserInfo />;
+  return (
+    <>
+      <UserInfo userId="123" />;
+      <UserInfo userId="234" />;
+      <UserInfo userId="345" />;
+    </>
+  );
 }
 
 export default App;
