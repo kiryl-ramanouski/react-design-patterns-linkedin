250ad1222a05a015776edd4840afd2114011983b
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-17 16:21:16 +0300

    feat: add updated components to App

--- react-patterns-app/src/App.js
+++ react-patterns-app/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import { UserInfo } from './UserInfo';
+import { ProductInfo } from './ProductInfo';
 
 function App() {
   return (
     <>
-      <UserInfo userId="123" />;
-      <UserInfo userId="234" />;
-      <UserInfo userId="345" />;
+      <UserInfo userId="123" />
+      <ProductInfo productId="2345" />
     </>
   );
 }
