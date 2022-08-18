0b799c8da30349b53a93d3d867736aedd4699cfa
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-18 10:44:55 +0300

    feat: prepare App component

--- react-patterns-app/src/App.js
+++ react-patterns-app/src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
-import { UserInfo } from './UserInfo';
-import { ProductInfo } from './ProductInfo';
+const nestedObject = {
+  a: 1,
+  b: {
+    b1: 4,
+    b2: {
+      b23: 'Hello',
+    },
+    b3: {
+      b31: {
+        message: 'Hi',
+      },
+      b32: {
+        message: 'Hi',
+      },
+    },
+  },
+  c: {
+    c1: 2,
+    c2: 3,
+  },
+};
 
 function App() {
-  return (
-    <>
-      <UserInfo userId="123" />
-      <ProductInfo productId="2345" />
-    </>
-  );
+  return <h1>Your code goes here</h1>;
 }
 
 export default App;
