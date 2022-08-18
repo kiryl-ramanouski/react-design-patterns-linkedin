b321808e4926543b0a6f1bb12dbc934ec05c42b0
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-18 12:39:29 +0300

    refactor: clear App

--- react-patterns-app/src/App.js
+++ react-patterns-app/src/App.js
@@ -1,29 +1,6 @@
 import React from 'react';
 
-import { RecursiveComponent } from './RecursiveComponent';
 import { Button, DangerButton, BigSuccessButton } from './partiallyApply';
-
-const nestedObject = {
-  a: 1,
-  b: {
-    b1: 4,
-    b2: {
-      b23: 'Hello',
-    },
-    b3: {
-      b31: {
-        message: 'Hi',
-      },
-      b32: {
-        message: 'Hi',
-      },
-    },
-  },
-  c: {
-    c1: 2,
-    c2: 3,
-  },
-};
 
 function App() {
   return (
